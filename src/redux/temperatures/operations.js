827a279b95f64833e429fcfbdc9491f1c940b0e1
Chris Zempel <zempel@efeqdev.com>
2018-05-28 19:15:58 -0500

    Refactors temperatures operations

--- src/redux/temperatures/operations.js
+++ src/redux/temperatures/operations.js
@@ -1,50 +1,29 @@
 import * as actions from "./actions.js";
+import { temperaturesQuery } from "./query.js";
 
-const fetchTemperatures = () => dispatch => {
-  dispatch(actions.temperaturesRequested());
-  fetch("http://OpenagBloom.ddns.net:5985/mvp_test/_find", {
+const fetchTemperatures = () => {
+  return async dispatch => {
+    dispatch(actions.temperaturesRequested());
+    const response = await temperatures();
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch(actions.temperaturesRequestSucceeded(json.docs, json.bookmark));
+    } else {
+      dispatch(actions.temperaturesRequestFailed());
+    }
+  };
+};
+
+const temperatures = () => {
+  return fetch("http://OpenagBloom.ddns.net:5985/mvp_test/_find", {
     method: "POST",
     credentials: "include",
     headers: {
       "content-type": "application/json"
     },
-    body: JSON.stringify({
-      selector: {
-        "start_date.timestamp": {
-          $gt: ""
-        },
-        "status.status_qualifier": {
-          $eq: "Success"
-        },
-        activity_type: {
-          $eq: "Environmental_Observation"
-        },
-        "subject.name": {
-          $eq: "Air"
-        },
-        "subject.location.name": {
-          $eq: "Left_Side"
-        },
-        "subject.attribute.name": {
-          $eq: "Temperature"
-        }
-      },
-      fields: ["start_date.timestamp", "subject.attribute.value"],
-      sort: [
-        {
-          "start_date.timestamp": "desc"
-        }
-      ],
-      limit: 72
-    })
-  })
-    .then(res => res.json())
-    .then(json => {
-      dispatch(actions.temperaturesRequestSucceeded(json.docs, json.bookmark));
-    })
-    .catch(error => {
-      dispatch(actions.temperaturesRequestFailed());
-    });
+    body: JSON.stringify(temperaturesQuery)
+  });
 };
 
 export { fetchTemperatures };
